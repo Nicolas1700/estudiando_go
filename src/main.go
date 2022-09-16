b5bac6dd62362838617311fd81c68b07a0404a26
Nicolas :) <nicolas.pc.1700@gmail.com>
2022-09-16 14:10:58 -0500

    Se aprende el uso de funciones, con uno a varios parametros

--- src/main.go
+++ src/main.go
@@ -2,6 +2,43 @@ package main
 
 import "fmt"
 
+func funNormal() {
+	fmt.Println("Hello word")
+}
+
+func funUnParametro(message string) {
+	fmt.Println(message)
+}
+
+func funVariosParametros(a, b int, c string) {
+	fmt.Println(a, b, c)
+}
+
+func retunrValue(a int) int {
+	return a * 2
+}
+
+func doubleValue(a int) (b, c int) {
+	return a, a * 2
+}
+
+// Funciones sobre encontrar el area de un rectangulo, tapecio y circulo
+// Rectangulo
+func areaRectangulo(baseRectangulo, alturaRectangulo float64) float64 {
+	return baseRectangulo * alturaRectangulo
+}
+
+// Trapecio
+func areaTrapecio(baseMenor, baseMayor, alturaTrapecio float64) float64 {
+	return (baseMenor + baseMayor) * alturaTrapecio / 2
+}
+
+// Circulo
+func areaCirculo(radio float64) float64 {
+	const pi float64 = 3.141592
+	return radio * radio * pi
+}
+
 func main() {
 
 	/*
@@ -90,29 +127,58 @@ func main() {
 		const pi float64 = 3.141592
 		areaCirculo := (radio * radio) * pi
 
-		fmt.Println("Area circulo: ", areaCirculo)*/
+		fmt.Println("Area circulo: ", areaCirculo)
+	*/
 
 	// Uso paquete fmt
-	helloMenssage := "Hello"
-	worldMenssage := "world"
+	/*
+		helloMenssage := "Hello"
+		worldMenssage := "world"
 
-	//Println
-	fmt.Println(helloMenssage, worldMenssage)
-	fmt.Println(helloMenssage, worldMenssage)
+		//Println
+		fmt.Println(helloMenssage, worldMenssage)
+		fmt.Println(helloMenssage, worldMenssage)
 
-	//Printf
-	nombre := "Nicolas"
-	edad := 18
+		//Printf
+		nombre := "Nicolas"
+		edad := 18
 
-	fmt.Printf("%s tiene %d años \n", nombre, edad)
-	fmt.Printf("%v tiene %v años \n", nombre, edad)
+		fmt.Printf("%s tiene %d años \n", nombre, edad)
+		fmt.Printf("%v tiene %v años \n", nombre, edad)
 
-	//Spintf
-	menssage := fmt.Sprintf("%s tiene %d años.", nombre, edad)
-	fmt.Println(menssage)
+		//Spintf
+		menssage := fmt.Sprintf("%s tiene %d años.", nombre, edad)
+		fmt.Println(menssage)
 
-	// Conocer tipo de dato
-	fmt.Printf("Tipo de nombre: %T \n", nombre)
-	fmt.Printf("Tipo de edad: %T \n", edad)
+		// Conocer tipo de dato
+		fmt.Printf("Tipo de nombre: %T \n", nombre)
+		fmt.Printf("Tipo de edad: %T \n", edad)
+	*/
+
+	//Uso de funciones
+	
+	//Una funcion sin parametros
+	funNormal()
+	//funcion con un parametro
+	funUnParametro("Hello world")
+	//Funcion con varios parametros
+	funVariosParametros(1, 2, "Hello")
+	// Funcion con return
+	value := retunrValue(2)
+	fmt.Println(value)
+	// Funcion con varios return
+	value1, _ := doubleValue(2)
+	fmt.Println("Value 1: ", value1)
+
+	//Reto funciones de areas de figuras geometricas
+	//Rectangulo
+	areaRectangulo := areaRectangulo(5, 8)
+	fmt.Println("Area Rectangulo: ", areaRectangulo)
+	//Trapecio
+	areaTrapecio := areaTrapecio(5, 6, 9)
+	fmt.Println("Area Trapecio: ", areaTrapecio)
+	//Ciculo
+	areaCirculo := areaCirculo(5)
+	fmt.Println("Area circulo: ", areaCirculo)
 
 }
